4c7f32604c95f3bab77427a05cc24b8f742fffac
rationalcode <marat_pochta@mail.ru>
2017-07-19 10:19:32 +0500

    1

---
+++ app/src/main/java/com/example/admin/justice/SelectMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.justice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SelectMenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_menu);
+    }
+}
